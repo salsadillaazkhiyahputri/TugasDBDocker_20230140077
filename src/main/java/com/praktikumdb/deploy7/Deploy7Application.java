18283b0782ccd6e47d05fb6e067bf2a03cfd6193
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-04-20 09:41:13 +0700

    Initial commit

---
+++ src/main/java/com/praktikumdb/deploy7/Deploy7Application.java
@@ -0,0 +1,13 @@
+package com.praktikumdb.deploy7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy7Application.class, args);
+	}
+
+}
